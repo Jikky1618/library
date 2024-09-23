56a8478ae3016c927139cbc0f018e85cc8482c37
Jikky <Jikky0110@gmail.com>
2024-09-23 21:06:13 +0900

    Add aplusb.text.cpp

---
+++ verify/aplusb.text.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << '\n';
+}
